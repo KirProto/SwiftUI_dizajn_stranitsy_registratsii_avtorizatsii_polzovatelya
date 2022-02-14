cebbed7e679bcb36f9ce1e39d10606ea67e373a6
Kir Proto Q <kir.proto@ya.ru>
2022-02-14 10:51:06 +0500

    Initial Commit

---
+++ Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelya/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelya
+//
+//  Created by KIR Q on 2022.02.14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelya/Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelyaApp.swift
+//  Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelya
+//
+//  Created by KIR Q on 2022.02.14.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

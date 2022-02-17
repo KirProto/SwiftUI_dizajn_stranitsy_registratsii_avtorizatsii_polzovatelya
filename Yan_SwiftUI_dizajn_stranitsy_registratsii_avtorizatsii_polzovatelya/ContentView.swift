de97d3f73aa2f169e155ac879c7cf45420a1145f
Kir Proto Q <kir.proto@ya.ru>
2022-02-17 10:14:53 +0500

    09:00 как сделать заливку изображением ?

--- Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelya/ContentView.swift
+++ Yan_SwiftUI_dizajn_stranitsy_registratsii_avtorizatsii_polzovatelya/ContentView.swift
@@ -5,12 +5,87 @@
 //  Created by KIR Q on 2022.02.14.
 //
 
+
+// pentagon.fill Logo
+// applelogo
+// twitter
+// facebook
+//
+//
+//
+
+
+
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { _ in
+            
+            VStack {
+                Image(systemName: "pentagon.fill")
+                    .resizable()
+                    .frame(width: 70, height: 70)
+//                    .foregroundStyle(.gray) //
+                
+                ZStack {
+                    Text("Hello Swift")
+                }
+                
+                HStack(spacing: 15) {
+                    Rectangle()
+                        .fill(Color("dg"))
+                        .frame(height: 1)
+                    
+                    Text("OR")
+                    
+                    Rectangle()
+                        .fill(Color("gr"))
+                        .frame(height: 1)
+                }
+                    .padding(.horizontal, 30)
+                    .padding(.top, 50)
+                
+                HStack(spacing: 25) {
+                    Button(action: {
+                        //
+                    }) {
+                        Image(systemName: "applelogo")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        //
+                    }) {
+                        Image("facebook")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        //
+                    }) {
+                        Image("Twitter")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                    
+                }
+                .padding(.top, 30)
+                
+            }
+            .padding(.vertical)
+        }
+        .background(Color("re").edgesIgnoringSafeArea(.all))
+        .preferredColorScheme(.dark)
     }
 }
 
